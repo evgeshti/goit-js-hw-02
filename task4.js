a944ed9ff3d5cf6f56df80071b8a2caa70f98f64
evgeshtm <evgeshti@gmail.com>
2020-07-30 22:25:52 +0300

    add terenary to task 4

--- task4.js
+++ task4.js
@@ -1,28 +1,26 @@
 const formatString = function (string) {
-  if (string.length <= 40) {
-    return string;
-  }
-  if (string.length > 40) {
-    const shortString = string.slice(0, 40) + '...';
-    return shortString;
-  }
+  const shortString = string.length > 40 ? string.slice(0, 40) + '...' : string;
+  return shortString;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
+// ПРЕДЫДУЩЕЕ решение
+
+// if (string.length <= 40) {
+//   return string;
+// }
+// if (string.length > 40) {
+//   const shortString = string.slice(0, 40) + '...';
+//   return shortString;
+// }
+
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// вернется оригинальная строка
 
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// вернется форматированная строка
 
 console.log(formatString('Curabitur ligula sapien.'));
-// вернется оригинальная строка
 
 console.log(
   formatString(
     'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
   ),
 );
-// вернется форматированная строка
